a6fb46baf89c1cee4efb65230c2c7bef27068768
Joao_Normando <69910609+Joao-Normando@users.noreply.github.com>
2021-04-24 12:31:25 -0300

    Update index.tsx

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,5 +1,7 @@
 import './styles.css';
+import {ReactComponent as YoutubeIcon, } from './youtube.svg';
 import {ReactComponent as LinkedinIcon, } from './linkedin.svg';
+import {ReactComponent as InstaIcon, } from './instagram.svg';
 
 
 
